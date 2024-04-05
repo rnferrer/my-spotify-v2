6e9ec522a17f746d40096250c83a6e92213f3f5b
rnferrer <rnferrer@ucdavis.edu>
2024-04-04 22:05:57 -0700

    added queue container and changed styling of drawer

--- src/client/components/MusicDrawer/MusicQueue/MusicQueue.tsx
+++ src/client/components/MusicDrawer/MusicQueue/MusicQueue.tsx
@@ -1,16 +1,95 @@
 import './MusicQueue.css'
 import { ScrollArea } from '@/components/ui/scroll-area'
+import { Separator } from '@/components/ui/separator'
+
+let dummyResults = [
+  // {
+  //   album: 'https://picsum.photos/50',
+  //   artist: 'Post Malone',
+  //   song: 'Goodbyes (ft. Young thug & Justin Beiber)'
+  // },
+  {
+    album: 'https://picsum.photos/50',
+    artist: 'Post Malone',
+    song: 'I Fall Apart'
+  },
+  {
+    album: 'https://picsum.photos/50',
+    artist: 'Post Malone',
+    song: 'White Iverson'
+  },
+  {
+    album: 'https://picsum.photos/50',
+    artist: 'Post Malone',
+    song: 'Sunflower'
+  },
+  {
+    album: 'https://picsum.photos/50',
+    artist: 'Post Malone',
+    song: 'Psycho'
+  },
+  {
+    album: 'https://picsum.photos/50',
+    artist: 'Post Malone',
+    song: 'Goodbyes'
+  },
+  {
+    album: 'https://picsum.photos/50',
+    artist: 'Post Malone',
+    song: 'I Fall Apart'
+  },
+  {
+    album: 'https://picsum.photos/50',
+    artist: 'Post Malone',
+    song: 'White Iverson'
+  },
+  {
+    album: 'https://picsum.photos/50',
+    artist: 'Post Malone',
+    song: 'Sunflower'
+  },
+  {
+    album: 'https://picsum.photos/50',
+    artist: 'Post Malone',
+    song: 'Psycho'
+  },
+]
 
 function MusicQueue() {
   return (
+    <>
+      <div id="music-queue-container">
+        <ScrollArea id="music-queue-list-container">
+          <strong>
+            <h1 id="music-queue-title">
+              Queue
+            </h1>
+          </strong>
+          {
+            dummyResults.map((result)=> {
+              return(
+                <>
+                  <div id="music-queue-list-item-container">
+                    <div id="music-queue-list-item-info">
+                      <div id="music-queue-list-image-container">
+                        <img src={result.album}></img>
+                      </div>
+                      <p>{`${result.artist} - ${result.song}`}</p>
 
-    <div id="music-queue-container">
-      <ScrollArea id="music-queue-list-container">
-        <h1>Queue</h1>
+                    </div>
+                  </div>
+                  <Separator/>
+                
+                </>
+              )
+            })
+          }
 
-      </ScrollArea>
 
-    </div>
+        </ScrollArea>
+
+      </div>
+    </>
 
   )
 }
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "relative flex h-[35px] w-[360px] rounded-lg text-white bg-[hsl(0,0%,4%)] px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring",
+          "relative flex h-[35px] w-[360px] rounded-lg text-white bg-[hsl(0,0%,0%)] px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring",
           className
         )}
         ref={ref}
