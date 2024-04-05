59b9a8999aad8d63906af576c07f8b23c70db7bb
rnferrer <rnferrer@ucdavis.edu>
2024-04-05 14:39:43 -0700

    added toast for dequeueing song

--- src/client/components/MusicDrawer/MusicQueue/MusicQueue.tsx
+++ src/client/components/MusicDrawer/MusicQueue/MusicQueue.tsx
@@ -1,6 +1,8 @@
 import './MusicQueue.css'
 import { ScrollArea } from '@/components/ui/scroll-area'
 import { Separator } from '@/components/ui/separator'
+import { useToast } from '@/components/ui/use-toast';
+
 
 let dummyResults = [
   // {
@@ -56,6 +58,17 @@ let dummyResults = [
 ]
 
 function MusicQueue() {
+
+  const { toast } = useToast();
+
+  const handleDequeue = (song:string) => {
+    return toast({
+      variant: 'destructive',
+      description: `${song} has been removed from queue`,
+      duration: 1500,
+    })
+  }
+
   return (
     <>
       <div id="music-queue-container">
@@ -77,6 +90,10 @@ function MusicQueue() {
                       <p>{`${result.artist} - ${result.song}`}</p>
 
                     </div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" id="music-queue-list-item-button" fill="white" viewBox="0 0 16 16" onClick={(e) => handleDequeue(result.song)}>
+                      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                      <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8"/>
+                    </svg>
                   </div>
                   <Separator/>
                 
--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -16,8 +16,8 @@ const Separator = React.forwardRef<
       decorative={decorative}
       orientation={orientation}
       className={cn(
-        "shrink-0 bg-border w-[70%]",
-        orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
+        "shrink-0 bg-[#727272] mx-5",
+        orientation === "horizontal" ? "h-[1px]" : "h-full w-[1px]",
         className
       )}
       {...props}
